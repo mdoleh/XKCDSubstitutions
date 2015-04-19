729a7995a5069e0fe4bee31aad3958d3adec5e4e
Mohammad Doleh <mdoleh@kent.edu>
2015-04-18 21:50:58 -0400

    Updated list

--- XKCDSubstitutions/XKCDSubstitutions/Controllers/HomeController.cs
+++ XKCDSubstitutions/XKCDSubstitutions/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
@@ -20,14 +22,25 @@ namespace XKCDSubstitutions.Controllers
         [HttpGet]
         public string GetContent(string url)
         {
+            url = cleanURL(url);
             var result = doGet(url);
-            return replaceWords(processGetResponse(result.Result, url));
+            return string.IsNullOrEmpty(result.Result) ? "" : replaceWords(processGetResponse(result.Result, url));
+        }
+
+        private string cleanURL(string url)
+        {
+            int index = url.LastIndexOf("/");
+            if (index != url.Length - 1)
+            {
+                url = url.Remove(index + 1, url.Length - index - 1);
+            }
+            return url;
         }
 
         private string replaceWords(string page)
         {
-            var wordsToReplace = new List<string>("democrats.republicans.witnesses.allegedy.new study.rebuild.space.google glass.smartphone.electric.senator.car.election.congressional leaders.homeland security.could not be reached for comment".Split('.'));
-            var replacementWords = new List<string>("monkeys.apes.these dudes I know.kinda probably.tumblr post.avenge.spaaace.virtual boy.pokedex.atomic.elf-lord.cat.eating contest.river spirits.homestar runner.is guilty and everyone knows it".Split('.'));
+            var wordsToReplace = new List<string>("weeks.EU.case against.microsoft.apple.google.billion.million.senate.democrats.republicans.witnesses.allegedy.new study.rebuild.space.google glass.smartphone.electric.senator.car.election.congressional leaders.homeland security.could not be reached for comment".Split('.'));
+            var replacementWords = new List<string>("fortnights.dat EU.duel against.a former Bill Gates company.a former Steve Jobs company.our lord and savior google.ba-jillion.zillion.jedi council.monkeys.giraffes.these dudes I know.kinda probably.tumblr post.avenge.spaaace.virtual boy.pokedex.atomic.elf-lord.cat.eating contest.river spirits.homestar runner.is guilty and everyone knows it".Split('.'));
             for (var i = 0; i < wordsToReplace.Count; ++i)
             {
                 page = ReplaceString(page, wordsToReplace[i], "<b>" + replacementWords[i] + "</b>", StringComparison.OrdinalIgnoreCase);
